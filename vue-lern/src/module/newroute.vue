c1bee3cb756cdaeaeb9db2dbd067191e81f2c338
SashaBazueva <zvezdopad659527@gmail.com>
2025-10-30 23:07:20 +0300

    Выравнивание анимации по центру экрана

--- vue-lern/src/module/newroute.vue
+++ vue-lern/src/module/newroute.vue
@@ -71,7 +71,7 @@ function handleButtonClick() {
         <div class="spinner">
             <div class="dot" v-for="n in 9" :key="n"></div>
         </div>
-        <p class="loading-text">Генерируем маршрут...</p>
+        <p class="loading-text">Загрузка...</p>
     </div>
   </transition>
 
@@ -181,9 +181,6 @@ function handleButtonClick() {
   }
 
   .loader-screen {
-  display: flex;
-  flex-direction: column;
-  gap: 57px;
   position: fixed;
   top: 0;
   left: 0;
@@ -191,19 +188,25 @@ function handleButtonClick() {
   height: 100%;
   background: white;
   display: flex;
-  justify-content: center;
-  align-items: center;
+  flex-direction: column; 
+  justify-content: center; 
+  align-items: center;     
+  text-align: center;
   z-index: 1000;
-}
-
-.loading-text{
-    font-size: 20px;
 }
 
 .spinner {
   position: relative;
   width: 170px;
   height: 170px;
+  margin-bottom: 50px;
+}
+
+.loading-text {
+  font-size: 20px;
+  color: #333;
+  text-align: center;
+  margin: 0;
 }
 
 .dot {
@@ -216,7 +219,7 @@ function handleButtonClick() {
 .dot:nth-child(1) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(0deg));
-  left: calc(50% + 48.5% * cos(0deg));
+  left: calc(38% + 48.5% * cos(0deg));
   margin-left: 0;
   animation-delay: 0s;
   transform: translate(-50%, -50%);
@@ -225,7 +228,7 @@ function handleButtonClick() {
 .dot:nth-child(2) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(40deg));
-  left: calc(50% + 48.5% * cos(40deg));
+  left: calc(38% + 48.5% * cos(40deg));
   animation-delay: 0.1s;
   transform: translate(-50%, -50%);
 }
@@ -233,7 +236,7 @@ function handleButtonClick() {
 .dot:nth-child(3) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(80deg));
-  left: calc(50% + 48.5% * cos(80deg));
+  left: calc(38% + 48.5% * cos(80deg));
   animation-delay: 0.2s;
   transform: translate(-50%, -50%);
 }
@@ -241,7 +244,7 @@ function handleButtonClick() {
 .dot:nth-child(4) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(120deg));
-  left: calc(50% + 48.5% * cos(120deg));
+  left: calc(38% + 48.5% * cos(120deg));
   animation-delay: 0.3s;
   transform: translate(-50%, -50%);
 }
@@ -249,7 +252,7 @@ function handleButtonClick() {
 .dot:nth-child(5) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(160deg));
-  left: calc(50% + 48.5% * cos(160deg));
+  left: calc(38% + 48.5% * cos(160deg));
   animation-delay: 0.4s;
   transform: translate(-50%, -50%);
 }
@@ -257,7 +260,7 @@ function handleButtonClick() {
 .dot:nth-child(6) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(200deg));
-  left: calc(50% + 48.5% * cos(200deg));
+  left: calc(38% + 48.5% * cos(200deg));
   animation-delay: 0.5s;
   transform: translate(-50%, -50%);
 }
@@ -265,7 +268,7 @@ function handleButtonClick() {
 .dot:nth-child(7) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(240deg));
-  left: calc(50% + 48.5% * cos(240deg));
+  left: calc(38% + 48.5% * cos(240deg));
   animation-delay: 0.6s;
   transform: translate(-50%, -50%);
 }
@@ -273,7 +276,7 @@ function handleButtonClick() {
 .dot:nth-child(8) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(280deg));
-  left: calc(50% + 48.5% * cos(280deg));
+  left: calc(38% + 48.5% * cos(280deg));
   animation-delay: 0.7s;
   transform: translate(-50%, -50%);
 }
@@ -281,7 +284,7 @@ function handleButtonClick() {
 .dot:nth-child(9) {
   width: 30px; height: 30px;
   top: calc(50% + 48.5% * sin(320deg));
-  left: calc(50% + 48.5% * cos(320deg));
+  left: calc(38% + 48.5% * cos(320deg));
   animation-delay: 0.8s;
   transform: translate(-50%, -50%);
 }
